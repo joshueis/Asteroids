0a594db95403fb5318a1c693a397b2158e075a68
Israel Carvajal <israelcarvajal@Israels-MacBook-Pro.local>
2014-12-03 16:11:41 -0700

    Initial Commit

---
+++ Asteroids/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Asteroids
+//
+//  Created by Israel Carvajal on 12/3/14.
+//  Copyright (c) 2014 Israel Carvajal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
